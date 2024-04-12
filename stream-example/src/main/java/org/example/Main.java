636a3381ee7f5595bc3fff64856bad3b9761429d
@aleksei_neverko <aaneverko@mail.ru>
2024-04-13 01:23:39 +0300

    initial commit task8

--- stream-example/src/main/java/org/example/Main.java
+++ stream-example/src/main/java/org/example/Main.java
@@ -15,13 +15,13 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) throws IOException {
 //        task1();
-        task2();
+//        task2();
 //        task3();
 //        task4();
 //        task5();
 //        task6();
 //        task7();
-//        task8();
+        task8();
 //        task9();
 //        task10();
 //        task11();
